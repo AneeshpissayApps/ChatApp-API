07d1c3bbaea38441de2e1d56073c687ef832eb59
AneeshpissayApps <109857472+AneeshpissayApps@users.noreply.github.com>
2022-07-23 21:20:34 +0530

    Changed mongodb uri

--- src/services/dbservice.js
+++ src/services/dbservice.js
@@ -1,6 +1,6 @@
 
 const mongoose = require("mongoose");
-const uri = "mongodb+srv://Aneeshpissay:Aneeshpissay@aneeshpissayapps.k1d5ube.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.MONGODBURI;
 const client = mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = client;
